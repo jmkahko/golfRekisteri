814c7c6b2a4251985f8a665368ee9b4439bcb01d
jmkahkon <janne.m.kahkonen@student.jyu.fi>
2022-07-23 19:01:56 +0300

    Implementoidaan GolfRekisteri

--- src/fxTuloskortti/LuoSeuraController.java
+++ src/fxTuloskortti/LuoSeuraController.java
@@ -4,13 +4,14 @@ import fi.jyu.mit.fxgui.Dialogs;
 import fi.jyu.mit.fxgui.ModalController;
 import fi.jyu.mit.fxgui.ModalControllerInterface;
 import javafx.fxml.FXML;
+import tuloskortti.GolfRekisteri;
 
 /**
  * @author janne
  * @version 13.6.2022
  * LuoSeura käyttöliittymän controllerit
  */
-public class LuoSeuraController implements ModalControllerInterface<String> {
+public class LuoSeuraController implements ModalControllerInterface<GolfRekisteri> {
 
     /**
      * Voidaan syöttää uusi tuloskortti
@@ -30,22 +31,34 @@ public class LuoSeuraController implements ModalControllerInterface<String> {
         Dialogs.showMessageDialog("Ei vielä osata tehdä");
     }
 
-    @Override
-    public String getResult() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+
 
     @Override
     public void handleShown() {
         // TODO Auto-generated method stub
         
-    }
+    }    
 
     @Override
-    public void setDefault(String oletus) {
+    public GolfRekisteri getResult() {
         // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    
+    @Override
+    public void setDefault(GolfRekisteri rekisteri) {
+        this.golfRekisteri = rekisteri;
+        
+        // Alustetaan tässä vaiheessa tässä tiedot;
+        
         
     }
+    
+ // =================================================================
+ // Tästä eteenpäin ei ole suoraan käyttöliittymään viittaavaa koodia
+
+    private GolfRekisteri golfRekisteri;
 
 }
