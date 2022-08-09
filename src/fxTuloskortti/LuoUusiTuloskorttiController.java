50dbaffff79f999890b73bc0cc1337708e057ece
jmkahkon <janne.m.kahkonen@student.jyu.fi>
2022-08-09 20:25:23 +0300

    Lisätty muokkaa tuloskorttia näkymään tiedot kovakoodattuna ensimmäinen seura aluksi

--- src/fxTuloskortti/LuoUusiTuloskorttiController.java
+++ src/fxTuloskortti/LuoUusiTuloskorttiController.java
@@ -1,10 +1,16 @@
 package fxTuloskortti;
 
+import java.util.List;
+
 import fi.jyu.mit.fxgui.Dialogs;
+import fi.jyu.mit.fxgui.ModalController;
 import fi.jyu.mit.fxgui.ModalControllerInterface;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import tuloskortti.GolfRekisteri;
+import tuloskortti.Seura;
+import tuloskortti.Tuloskortti;
 
 /**
  * @author Janne Kähkönen
@@ -170,10 +176,10 @@ public class LuoUusiTuloskorttiController implements ModalControllerInterface<Go
     @Override
     public void handleShown() {
         // TODO Auto-generated method stub
+        naytaTuloskortti(tuloskorttiEdits, this.golfRekisteri.annaSeura(0));
         
-    }
-
-
+    }  
+    
     @Override
     public GolfRekisteri getResult() {
         // TODO Auto-generated method stub
@@ -181,15 +187,101 @@ public class LuoUusiTuloskorttiController implements ModalControllerInterface<Go
     }
 
     @Override
-    public void setDefault(GolfRekisteri rekisteri) {
-        this.golfRekisteri = rekisteri;
-
-        
+    public void setDefault(GolfRekisteri golfRekisteri) {
+        this.golfRekisteri = golfRekisteri;
+        alusta();
     }
+
+
     
     // =================================================================
     // Tästä eteenpäin ei ole suoraan käyttöliittymään viittaavaa koodia
     
     private GolfRekisteri golfRekisteri;
+    private TextField[] tuloskorttiEdits;
+    private Seura seura;
+    
+    
+    /**
+     * Alustetaan tuloskortin tiedot
+     */
+    protected void alusta() {
+        TextField[] tuloskorttiEdits = {
+                vayla1_48, vayla1_51, vayla1_55, vayla1_62, vayla1_hcp, vayla1_par,
+                vayla2_48, vayla2_51, vayla2_55, vayla2_62, vayla2_hcp, vayla2_par,
+                vayla3_48, vayla3_51, vayla3_55, vayla3_62, vayla3_hcp, vayla3_par,
+                vayla4_48, vayla4_51, vayla4_55, vayla4_62, vayla4_hcp, vayla4_par,
+                vayla5_48, vayla5_51, vayla5_55, vayla5_62, vayla5_hcp, vayla5_par,
+                vayla6_48, vayla6_51, vayla6_55, vayla6_62, vayla6_hcp, vayla6_par,
+                vayla7_48, vayla7_51, vayla7_55, vayla7_62, vayla7_hcp, vayla7_par,
+                vayla8_48, vayla8_51, vayla8_55, vayla8_62, vayla8_hcp, vayla8_par,
+                vayla9_48, vayla9_51, vayla9_55, vayla9_62, vayla9_hcp, vayla9_par,
+                vayla10_48, vayla10_51, vayla10_55, vayla10_62, vayla10_hcp, vayla10_par,
+                vayla11_48, vayla11_51, vayla11_55, vayla11_62, vayla11_hcp, vayla11_par,
+                vayla12_48, vayla12_51, vayla12_55, vayla12_62, vayla12_hcp, vayla12_par,
+                vayla13_48, vayla13_51, vayla13_55, vayla13_62, vayla13_hcp, vayla13_par,
+                vayla14_48, vayla14_51, vayla14_55, vayla14_62, vayla14_hcp, vayla14_par,
+                vayla15_48, vayla15_51, vayla15_55, vayla15_62, vayla15_hcp, vayla15_par,
+                vayla16_48, vayla16_51, vayla16_55, vayla16_62, vayla16_hcp, vayla16_par,
+                vayla17_48, vayla17_51, vayla17_55, vayla17_62, vayla17_hcp, vayla17_par,
+                vayla18_48, vayla18_51, vayla18_55, vayla18_62, vayla18_hcp, vayla18_par};
+        this.tuloskorttiEdits = tuloskorttiEdits;
+    }
+    
+    /**
+     * Luodaan tuloskortin muokkaus dialogi ja palautetaan sama tietue muutettuna tai null arvo
+     * @param modalityStage mille ollaan modaalisia, null = sovellukselle
+     * @param seura mitä dataa näytetään oletuksena, kun muokataan tuloskorttia, niin olemassa olevan tuloskortin tiedot
+     * @return null jos painetaan Peruuta, muuten täytetään tietue
+     */
+    public static Seura kysyTuloskortti(Stage modalityStage, Seura seura) {
+        return ModalController.showModal(SeuraController.class.getResource("LuoUusiTuloskorttiView.fxml"), "Muokkaa tuloskorttia", modalityStage, seura);
+    }
+    
+    /**
+     * Hakee seuran tuloskortin joka on valittuna seura listalta
+     * @param edits viedään näytettävät kentät
+     * @param s jonka tuloskortti näytetään
+     */
+    public void naytaTuloskortti(TextField[] edits, Seura s) {
+        if (s == null) {
+            return;
+        }
+        
+        List<Tuloskortti> tuloskortti = golfRekisteri.annaTuloskortti(s);
+        
+        int pituus48 = 0;
+        int pituus51 = 0;
+        int pituus55 = 0;
+        int pituus62 = 0;
+        int par = 0;
 
+        int x = 0;
+        for (Tuloskortti t : tuloskortti) {
+            // Lisätään textField sarakkeisiin tuloskortin tiedot
+            edits[x].setText(String.valueOf(t.getPituus48()));
+            pituus48 += t.getPituus48();
+            x++;
+            edits[x].setText(String.valueOf(t.getPituus51()));
+            pituus51 += t.getPituus51();
+            x++;
+            edits[x].setText(String.valueOf(t.getPituus55()));
+            pituus55 += t.getPituus55();
+            x++;
+            edits[x].setText(String.valueOf(t.getPituus62()));
+            pituus62 += t.getPituus62();
+            x++;
+            edits[x].setText(String.valueOf(t.getHcp()));
+            x++;
+            edits[x].setText(String.valueOf(t.getPar()));
+            par += t.getPar();
+            x++;
+        }
+        
+        pituusYhteensa48.setText(String.valueOf(pituus48));
+        pituusYhteensa51.setText(String.valueOf(pituus51));
+        pituusYhteensa55.setText(String.valueOf(pituus55));
+        pituusYhteensa62.setText(String.valueOf(pituus62));
+        parYhteensa.setText(String.valueOf(par));
+    }    
 }
